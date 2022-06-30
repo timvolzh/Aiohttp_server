8956665ef996e6f83e97f3ea1a6edf6ddb4ab612
timvolzh <nigaman95@gmail.com>
2022-06-30 18:17:20 +0300

    Adde POST and GET

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ db = Gino()
 
 '''Models'''
 
+
 class AnnounceModel(db.Model):
     __tablename__ = 'announce'
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +16,14 @@ class AnnounceModel(db.Model):
     description = db.Column(db.String(1000), nullable=False)
     user = db.Column(db.Integer, nullable=False, default=1)
     create_time = db.Column(db.DateTime, server_default=db.func.now())
+
+    async def to_dict(self):
+        return {'id': self.id,
+                'create_time': str(self.create_time),
+                'title': self.title,
+                'description': self.description,
+                'user': self.user,
+                }
 
 
 async def orm_context(app):
@@ -26,10 +35,27 @@ async def orm_context(app):
     print('Finish')
 
 
+'''Views'''
+
+
 class AnnounceView(web.View):
 
     async def get(self):
+        announce_id = int(self.request.match_info['announce_id'])
+        announce = await AnnounceModel.get(announce_id)
+        if not announce:
+            return web.json_response({'Response': 'zhopa'})
+        return web.json_response(await announce.to_dict())
 
+    async def post(self):
+        announce_data = await self.request.json()
+        new_announce = await AnnounceModel.create(**announce_data)
+        return web.json_response({'id': new_announce.id})
+
+    async def path(self):
+        pass
+
+    async def delete(self):
         pass
 
 
